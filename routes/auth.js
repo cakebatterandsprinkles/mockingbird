4122a3553d3566df9b8c2a5849e1d564e7786d9b
yagmurcetintas <yagmurcetin@gmail.com>
2020-06-26 21:03:14 -0400

    added authentication routes

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+// @route  POST /signup
+// @desc   Register User
+// @access public
+
+router.post("/signup", authController.postSignup);
+
+// @route  POST /login
+// @desc   Login User
+// @access public
+
+router.post("/login", authController.postLogin);
+
+// @route  POST /logout
+// @desc   Logout User
+// @access private
+
+router.post("/logout", verifyToken, authController.postLogout);
+
+// @route  POST /settings
+// @desc   Change name and/or password of the User
+// @access private
+
+router.post("/settings", verifyToken, authController.postSettings);
+
+module.exports = router;
