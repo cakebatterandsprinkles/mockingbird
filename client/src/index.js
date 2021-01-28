d50273eea8216f7a166dd9249c766593108b9910
yagmurcetintas <yagmurcetin@gmail.com>
2021-01-28 09:34:58 -0500

    Deleted the redux store provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./store/reducers/rootReducer";
-import * as serviceWorker from "./serviceWorker";
 import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    {" "}
-    <App />{" "}
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
