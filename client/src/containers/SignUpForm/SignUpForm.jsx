00ef7648d994d92da1735964cb87d4b9aaf457b4
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-16 10:42:20 -0400

    Added show password feature to the signup form

--- client/src/containers/SignUpForm/SignUpForm.jsx
+++ client/src/containers/SignUpForm/SignUpForm.jsx
@@ -1,13 +1,35 @@
 import axios from "axios";
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router";
 import { toast } from "react-toastify";
+import { ReactComponent as Eye } from "../../assets/eye.svg";
+import { ReactComponent as EyeSlash } from "../../assets/eyeSlash.svg";
 import classes from "./SignUpForm.module.css";
 
 const SignUpForm = (props) => {
   const history = useHistory();
   const { register, handleSubmit, errors, watch } = useForm();
+  const [inputType, setInputType] = useState("password");
+  const [inputTypeRepeat, setInputTypeRepeat] = useState("password");
+  const passwordRef = useRef(null);
+  const repeatPasswordRef = useRef(null);
+
+  const handleShowPassword = () => {
+    if (passwordRef.current.type === "password") {
+      setInputType("text");
+    } else if (passwordRef.current.type === "text") {
+      setInputType("password");
+    }
+  };
+
+  const handleShowPasswordRepeat = () => {
+    if (repeatPasswordRef.current.type === "password") {
+      setInputTypeRepeat("text");
+    } else if (repeatPasswordRef.current.type === "text") {
+      setInputTypeRepeat("password");
+    }
+  };
 
   const onSubmit = (data, e) => {
     axios
@@ -68,55 +90,87 @@ const SignUpForm = (props) => {
             )}
           </div>
           <div className={classes.formGroupContainer}>
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              ref={register({ required: true, minLength: 8 })}
-            ></input>
-            {errors.password && errors.password.type === "required" && (
-              <span className={classes.error}>Password field is required.</span>
-            )}
-            {errors.password && errors.password.type === "minLength" && (
-              <span className={classes.error}>
-                The password has to be at least 8 characters long.
-              </span>
-            )}
-          </div>
-          <div className={classes.formGroupContainer}>
-            <label>Repeat Password:</label>
-            <input
-              type="password"
-              name="repeatPassword"
-              ref={register({
-                required: true,
-                minLength: 8,
-                validate: (value) => value === watch("password"),
-              })}
-            ></input>
-            {errors.repeatPassword &&
-              errors.repeatPassword.type === "required" && (
+            <label htmlFor="password">Password:</label>
+            <div className={classes.inputWrapper}>
+              <input
+                type={inputType}
+                name="password"
+                id="password"
+                ref={(ref) => {
+                  passwordRef.current = ref;
+                  register(ref, { required: true, minLength: 8 });
+                }}
+              ></input>
+              {errors.password && errors.password.type === "required" && (
                 <span className={classes.error}>
-                  Repeat password field is required.
+                  Password field is required.
                 </span>
               )}
-            {errors.repeatPassword &&
-              errors.repeatPassword.type === "minLength" && (
+              {errors.password && errors.password.type === "minLength" && (
                 <span className={classes.error}>
                   The password has to be at least 8 characters long.
                 </span>
               )}
-            {errors.repeatPassword &&
-              errors.repeatPassword.type === "validate" && (
-                <span className={classes.error}>The passwords must match.</span>
+              {passwordRef.current?.type === "password" ? (
+                <EyeSlash
+                  className={classes.eyeslash}
+                  onClick={handleShowPassword}
+                />
+              ) : (
+                <Eye className={classes.eye} onClick={handleShowPassword} />
               )}
+            </div>
+          </div>
+          <div className={classes.formGroupContainer}>
+            <label htmlFor="repeatPassword">Repeat Password:</label>
+            <div className={classes.inputWrapper}>
+              <input
+                type={inputTypeRepeat}
+                name="repeatPassword"
+                ref={(ref) => {
+                  repeatPasswordRef.current = ref;
+                  register(ref, {
+                    required: true,
+                    minLength: 8,
+                    validate: (value) => value === watch("password"),
+                  });
+                }}
+              ></input>
+              {errors.repeatPassword &&
+                errors.repeatPassword.type === "required" && (
+                  <span className={classes.error}>
+                    Repeat password field is required.
+                  </span>
+                )}
+              {errors.repeatPassword &&
+                errors.repeatPassword.type === "minLength" && (
+                  <span className={classes.error}>
+                    The password has to be at least 8 characters long.
+                  </span>
+                )}
+              {errors.repeatPassword &&
+                errors.repeatPassword.type === "validate" && (
+                  <span className={classes.error}>
+                    The passwords must match.
+                  </span>
+                )}
+              {repeatPasswordRef.current?.type === "password" ? (
+                <EyeSlash
+                  className={classes.eyeslash}
+                  onClick={handleShowPasswordRepeat}
+                />
+              ) : (
+                <Eye
+                  className={classes.eye}
+                  onClick={handleShowPasswordRepeat}
+                />
+              )}
+            </div>
           </div>
           <div className={classes.btnWrapper}>
-            <input
-              type="submit"
-              className={classes.link}
-              value="٩(◕‿◕｡)۶ Sign Me Up"
-            ></input>
+            <button className={classes.button} type="submit">
+              ٩(◕‿◕｡)۶ Sign Me Up
+            </button>
           </div>
         </form>
       </div>
