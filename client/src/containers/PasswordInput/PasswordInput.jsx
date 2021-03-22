da56233f1c8599c7e00139c67eefc63b6139270c
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-22 13:47:19 -0400

    Separated the password input component and integrated it to the signup form

--- client/src/containers/PasswordInput/PasswordInput.jsx
+++ client/src/containers/PasswordInput/PasswordInput.jsx
@@ -1,39 +1,84 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useState } from "react";
+import { useFormContext } from "react-hook-form";
 import { ReactComponent as Eye } from "../../assets/eye.svg";
 import { ReactComponent as EyeSlash } from "../../assets/eyeSlash.svg";
 import classes from "./PasswordInput.module.css";
 
-const PasswordInput = (props) => {
+const PasswordInput = ({
+  label,
+  errorRequired,
+  errorMinLength,
+  errorValidate,
+  name,
+}) => {
   const [password, setPassword] = useState("");
   const [inputType, setInputType] = useState("password");
-  const showPasswordRef = useRef(null);
+
+  const { errors, register, watch } = useFormContext();
 
   const handleInputChange = (event) => {
     setPassword(event.target.value);
-    props.handleChange(event.target.value);
   };
 
   const handleShowPassword = () => {
-    if (showPasswordRef.current.type === "password") {
-      setInputType("text");
-    } else if (showPasswordRef.current.type === "text") {
-      setInputType("password");
-    }
+    setInputType((currentState) =>
+      currentState === "text" ? "password" : "text"
+    );
   };
+
+  console.log(errors);
+
+  let registerRules = useMemo(() => {
+    const rules = {};
+    if (errorMinLength) {
+      rules.minLength = 9;
+    }
+    if (errorRequired) {
+      rules.required = true;
+    }
+    if (errorValidate) {
+      rules.validate = (value) => value === watch("password");
+    }
+
+    return rules;
+  }, [watch, errorMinLength, errorValidate, errorRequired]);
 
   return (
     <div className={classes.formGroupContainer}>
-      <label htmlFor="currentPassword">{props.label}</label>
-      <div className={classes.inputWrapper}>
+      <label htmlFor="currentPassword">{label}</label>
+      <span className={classes.inputWrapper}>
         <input
           type={inputType}
-          name="currentPassword"
-          ref={showPasswordRef}
+          name={name}
+          ref={register(registerRules)}
           onChange={handleInputChange}
           value={password}
         ></input>
-        <div onClick={handleShowPassword} className={classes.iconWrapper}>
-          {showPasswordRef.current?.type === "password" ? (
+        {errorRequired ? (
+          <span>
+            {errors.password && errors.password.type === "required" && (
+              <div className={classes.error}>Password field is required.</div>
+            )}{" "}
+          </span>
+        ) : null}
+        {errorMinLength ? (
+          <span>
+            {errors.password && errors.password.type === "minLength" && (
+              <div className={classes.error}>
+                The password has to be at least 8 characters long.
+              </div>
+            )}
+          </span>
+        ) : null}
+        {errorValidate ? (
+          <span>
+            {errors.password && errors.password.type === "validate" && (
+              <div className={classes.error}>The passwords must match.</div>
+            )}
+          </span>
+        ) : null}
+        <div className={classes.iconWrapper}>
+          {inputType === "text" ? (
             <EyeSlash
               className={classes.eyeslash}
               onClick={handleShowPassword}
@@ -42,7 +87,7 @@ const PasswordInput = (props) => {
             <Eye className={classes.eye} onClick={handleShowPassword} />
           )}
         </div>
-      </div>
+      </span>
     </div>
   );
 };
--- client/src/containers/SignUpForm/SignUpForm.jsx
+++ client/src/containers/SignUpForm/SignUpForm.jsx
@@ -1,34 +1,19 @@
 import axios from "axios";
-import React, { Fragment, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { Fragment } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { useHistory } from "react-router";
 import { toast } from "react-toastify";
-import { ReactComponent as Eye } from "../../assets/eye.svg";
-import { ReactComponent as EyeSlash } from "../../assets/eyeSlash.svg";
 import Dots from "../../components/Dots/Dots";
+import PasswordInput from "../PasswordInput/PasswordInput";
 import classes from "./SignUpForm.module.css";
 
-const SignUpForm = (props) => {
+const SignUpForm = () => {
   const history = useHistory();
-  const { register, handleSubmit, errors, watch } = useForm();
-  const [inputType, setInputType] = useState("password");
-  const [inputTypeRepeat, setInputTypeRepeat] = useState("password");
-  const passwordRef = useRef(null);
-  const repeatPasswordRef = useRef(null);
-
-  const handleShowPassword = () => {
-    setInputType((currentState) =>
-      currentState === "text" ? "password" : "text"
-    );
-  };
-
-  const handleShowPasswordRepeat = () => {
-    setInputTypeRepeat((currentState) =>
-      currentState === "text" ? "password" : "text"
-    );
-  };
+  const methods = useForm();
+  const { register, handleSubmit, errors } = methods;
 
   const onSubmit = (data, e) => {
+    console.log(data);
     axios
       .post("/signup", {
         email: data.email,
@@ -60,101 +45,43 @@ const SignUpForm = (props) => {
         <div className={classes.headingContainer}>
           <p className={classes.heading}>Sign Up</p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className={classes.formGroupContainer}>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              ref={register({ required: true })}
-            ></input>
-            <div className={classes.subTextContainer}>
-              Note: You will need to verify this. Make it real.
-            </div>
-            {errors.email && (
-              <div className={classes.error}>Email is required.</div>
-            )}
-          </div>
-          <div className={classes.formGroupContainer}>
-            <label htmlFor="password">Password:</label>
-            <div className={classes.inputWrapper}>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={classes.formGroupContainer}>
+              <label>Email:</label>
               <input
-                type={inputType}
-                name="password"
-                id="password"
-                ref={(ref) => {
-                  passwordRef.current = ref;
-                  register(ref, { required: true, minLength: 8 });
-                }}
+                type="email"
+                name="email"
+                ref={register({ required: true })}
               ></input>
-              {errors.password && errors.password.type === "required" && (
-                <div className={classes.error}>Password field is required.</div>
-              )}
-              {errors.password && errors.password.type === "minLength" && (
-                <div className={classes.error}>
-                  The password has to be at least 8 characters long.
-                </div>
-              )}
-              {inputType === "password" ? (
-                <EyeSlash
-                  className={classes.eyeslash}
-                  onClick={handleShowPassword}
-                />
-              ) : (
-                <Eye className={classes.eye} onClick={handleShowPassword} />
+              <div className={classes.subTextContainer}>
+                Note: You will need to verify this. Make it real.
+              </div>
+              {errors.email && (
+                <div className={classes.error}>Email is required.</div>
               )}
             </div>
-          </div>
-          <div className={classes.formGroupContainer}>
-            <label htmlFor="repeatPassword">Repeat Password:</label>
-            <div className={classes.inputWrapper}>
-              <input
-                type={inputTypeRepeat}
-                name="repeatPassword"
-                ref={(ref) => {
-                  repeatPasswordRef.current = ref;
-                  register(ref, {
-                    required: true,
-                    minLength: 8,
-                    validate: (value) => value === watch("password"),
-                  });
-                }}
-              ></input>
-              {errors.repeatPassword &&
-                errors.repeatPassword.type === "required" && (
-                  <div className={classes.error}>
-                    Repeat password field is required.
-                  </div>
-                )}
-              {errors.repeatPassword &&
-                errors.repeatPassword.type === "minLength" && (
-                  <div className={classes.error}>
-                    The password has to be at least 8 characters long.
-                  </div>
-                )}
-              {errors.repeatPassword &&
-                errors.repeatPassword.type === "validate" && (
-                  <div className={classes.error}>The passwords must match.</div>
-                )}
-              {inputTypeRepeat === "password" ? (
-                <EyeSlash
-                  className={classes.eyeslash}
-                  onClick={handleShowPasswordRepeat}
-                />
-              ) : (
-                <Eye
-                  className={classes.eye}
-                  onClick={handleShowPasswordRepeat}
-                />
-              )}
+            <PasswordInput
+              label="Password:"
+              errorRequired={true}
+              errorMinLength={true}
+              errorValidate={false}
+              name="password"
+            />
+            <PasswordInput
+              label="Repeat Password:"
+              errorRequired={true}
+              errorMinLength={true}
+              errorValidate={true}
+              name="repeatPassword"
+            />
+            <div className={classes.btnWrapper}>
+              <button className={classes.button} type="submit">
+                ٩(◕‿◕｡)۶ Sign Me Up
+              </button>
             </div>
-          </div>
-          <div className={classes.btnWrapper}>
-            <button className={classes.button} type="submit">
-              ٩(◕‿◕｡)۶ Sign Me Up
-            </button>
-          </div>
-        </form>
+          </form>
+        </FormProvider>
       </div>
     </Fragment>
   );
