0c7107fca30391396a202c2b51a15e3081390dfb
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-22 14:12:08 -0400

    Fixed bugs and changed style

--- client/src/containers/PasswordInput/PasswordInput.jsx
+++ client/src/containers/PasswordInput/PasswordInput.jsx
@@ -11,22 +11,15 @@ const PasswordInput = ({
   errorValidate,
   name,
 }) => {
-  const [password, setPassword] = useState("");
   const [inputType, setInputType] = useState("password");
 
   const { errors, register, watch } = useFormContext();
-
-  const handleInputChange = (event) => {
-    setPassword(event.target.value);
-  };
 
   const handleShowPassword = () => {
     setInputType((currentState) =>
       currentState === "text" ? "password" : "text"
     );
   };
-
-  console.log(errors);
 
   let registerRules = useMemo(() => {
     const rules = {};
@@ -46,37 +39,12 @@ const PasswordInput = ({
   return (
     <div className={classes.formGroupContainer}>
       <label htmlFor="currentPassword">{label}</label>
-      <span className={classes.inputWrapper}>
+      <div className={classes.inputWrapper}>
         <input
           type={inputType}
           name={name}
           ref={register(registerRules)}
-          onChange={handleInputChange}
-          value={password}
         ></input>
-        {errorRequired ? (
-          <span>
-            {errors.password && errors.password.type === "required" && (
-              <div className={classes.error}>Password field is required.</div>
-            )}{" "}
-          </span>
-        ) : null}
-        {errorMinLength ? (
-          <span>
-            {errors.password && errors.password.type === "minLength" && (
-              <div className={classes.error}>
-                The password has to be at least 8 characters long.
-              </div>
-            )}
-          </span>
-        ) : null}
-        {errorValidate ? (
-          <span>
-            {errors.password && errors.password.type === "validate" && (
-              <div className={classes.error}>The passwords must match.</div>
-            )}
-          </span>
-        ) : null}
         <div className={classes.iconWrapper}>
           {inputType === "text" ? (
             <EyeSlash
@@ -87,7 +55,30 @@ const PasswordInput = ({
             <Eye className={classes.eye} onClick={handleShowPassword} />
           )}
         </div>
-      </span>
+      </div>
+      {errorRequired ? (
+        <span>
+          {errors[name] && errors[name].type === "required" && (
+            <div className={classes.error}>Password field is required.</div>
+          )}{" "}
+        </span>
+      ) : null}
+      {errorMinLength ? (
+        <span>
+          {errors[name] && errors[name].type === "minLength" && (
+            <div className={classes.error}>
+              The password has to be at least 8 characters long.
+            </div>
+          )}
+        </span>
+      ) : null}
+      {errorValidate ? (
+        <span>
+          {errors[name] && errors[name].type === "validate" && (
+            <div className={classes.error}>The passwords must match.</div>
+          )}
+        </span>
+      ) : null}
     </div>
   );
 };
