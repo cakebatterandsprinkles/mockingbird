33b345c0296997635fd09d632d53d0de724bb8f1
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-23 15:48:14 -0400

    Integrated the PasswordInput component to the password reset form

--- client/src/containers/ResetPassword/ResetPassword.jsx
+++ client/src/containers/ResetPassword/ResetPassword.jsx
@@ -1,33 +1,17 @@
 import axios from "axios";
-import React, { Fragment, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { Fragment } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { useHistory, useLocation } from "react-router";
 import { toast } from "react-toastify";
-import { ReactComponent as Eye } from "../../assets/eye.svg";
-import { ReactComponent as EyeSlash } from "../../assets/eyeSlash.svg";
 import Dots from "../../components/Dots/Dots";
+import PasswordInput from "../PasswordInput/PasswordInput";
 import classes from "./ResetPassword.module.css";
 
 const ResetPassword = () => {
   const history = useHistory();
+  const methods = useForm();
+  const { handleSubmit } = methods;
   let location = new URLSearchParams(useLocation().search);
-  const { register, handleSubmit, errors, watch } = useForm();
-  const [inputType, setInputType] = useState("password");
-  const [inputTypeRepeat, setInputTypeRepeat] = useState("password");
-  const passwordRef = useRef(null);
-  const repeatPasswordRef = useRef(null);
-
-  const handleShowPassword = () => {
-    setInputType((currentState) =>
-      currentState === "text" ? "password" : "text"
-    );
-  };
-
-  const handleShowPasswordRepeat = () => {
-    setInputTypeRepeat((currentState) =>
-      currentState === "text" ? "password" : "text"
-    );
-  };
 
   const onSubmit = (data, e) => {
     axios
@@ -60,91 +44,35 @@ const ResetPassword = () => {
         <div className={classes.headingContainer}>
           <p className={classes.heading}>Reset Password</p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div
-            className={`${classes.formGroupContainer} ${classes.marginBottom}`}
-          >
-            <label htmlFor="password">New Password:</label>
-            <div className={classes.inputWrapper}>
-              <input
-                type={inputType}
-                name="password"
-                id="password"
-                ref={(ref) => {
-                  passwordRef.current = ref;
-                  register(ref, { required: true, minLength: 8 });
-                }}
-              ></input>
-              {errors.password && errors.password.type === "required" && (
-                <div className={classes.error}>Password field is required.</div>
-              )}
-              {errors.password && errors.password.type === "minLength" && (
-                <div className={classes.error}>
-                  The password has to be at least 8 characters long.
-                </div>
-              )}
-              {inputType ? (
-                <EyeSlash
-                  className={classes.eyeslash}
-                  onClick={handleShowPassword}
-                />
-              ) : (
-                <Eye className={classes.eye} onClick={handleShowPassword} />
-              )}
-            </div>
-            <div className={classes.formGroupContainer}>
-              <label htmlFor="repeatPassword">Repeat Password:</label>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div
+              className={`${classes.formGroupContainer} ${classes.marginBottom}`}
+            >
               <div className={classes.inputWrapper}>
-                <input
-                  type={inputTypeRepeat}
+                <PasswordInput
+                  label="New Password:"
+                  errorRequired={true}
+                  errorMinLength={true}
+                  errorValidate={false}
+                  name="password"
+                />
+                <PasswordInput
+                  label="Repeat New Password:"
+                  errorRequired={true}
+                  errorMinLength={true}
+                  errorValidate={true}
                   name="repeatPassword"
-                  ref={(ref) => {
-                    repeatPasswordRef.current = ref;
-                    register(ref, {
-                      required: true,
-                      minLength: 8,
-                      validate: (value) => value === watch("password"),
-                    });
-                  }}
-                ></input>
-                {errors.repeatPassword &&
-                  errors.repeatPassword.type === "required" && (
-                    <div className={classes.error}>
-                      Repeat password field is required.
-                    </div>
-                  )}
-                {errors.repeatPassword &&
-                  errors.repeatPassword.type === "minLength" && (
-                    <div className={classes.error}>
-                      The password has to be at least 8 characters long.
-                    </div>
-                  )}
-                {errors.repeatPassword &&
-                  errors.repeatPassword.type === "validate" && (
-                    <div className={classes.error}>
-                      The passwords must match.
-                    </div>
-                  )}
-                {inputTypeRepeat ? (
-                  <EyeSlash
-                    className={classes.eyeslash}
-                    onClick={handleShowPasswordRepeat}
-                  />
-                ) : (
-                  <Eye
-                    className={classes.eye}
-                    onClick={handleShowPasswordRepeat}
-                  />
-                )}
+                />
               </div>
             </div>
-          </div>
-          <div className={classes.btnWrapper}>
-            <button className={classes.button} type="submit">
-              Reset Password
-            </button>
-          </div>
-        </form>
+            <div className={classes.btnWrapper}>
+              <button className={classes.button} type="submit">
+                Reset Password
+              </button>
+            </div>
+          </form>
+        </FormProvider>
       </div>
     </Fragment>
   );
