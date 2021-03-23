f7c59b1970159af5068799cbc65b33e1ab648998
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-23 15:37:58 -0400

    Integrated the PasswordInput component to the login form

--- client/src/containers/LoginForm/LoginForm.jsx
+++ client/src/containers/LoginForm/LoginForm.jsx
@@ -1,33 +1,24 @@
 import axios from "axios";
-import React, { Fragment, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { Fragment, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import Modal from "react-modal";
 import { useHistory } from "react-router";
 import { toast } from "react-toastify";
 import { ReactComponent as ClosingButton } from "../../assets/closeButton.svg";
-import { ReactComponent as Eye } from "../../assets/eye.svg";
-import { ReactComponent as EyeSlash } from "../../assets/eyeSlash.svg";
 import Dots from "../../components/Dots/Dots";
 import { useUserContext } from "../../context/authContext";
+import PasswordInput from "../PasswordInput/PasswordInput";
 import classes from "./LoginForm.module.css";
 
 const LoginForm = () => {
   const history = useHistory();
-  const { register, handleSubmit, errors } = useForm();
-  const [inputType, setInputType] = useState("password");
+  const methods = useForm();
+  const { register, handleSubmit, errors } = methods;
   const [showModal, setShowModal] = useState(false);
   const [email, setEmail] = useState("");
   const { setUserData } = useUserContext();
-  const passwordRef = useRef(null);
-
-  const handleShowPassword = () => {
-    setInputType((currentState) =>
-      currentState === "text" ? "password" : "text"
-    );
-  };
 
   const handleOpenModal = () => setShowModal(true);
-
   const handleCloseModal = () => setShowModal(false);
 
   const handleForgotPasswordSubmit = (event) => {
@@ -79,57 +70,40 @@ const LoginForm = () => {
         <div className={classes.headingContainer}>
           <p className={classes.heading}>Login</p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className={classes.formGroupContainer}>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              ref={register({ required: true })}
-            ></input>
-            {errors.email && (
-              <span className={classes.error}>Email is required.</span>
-            )}
-          </div>
-          <div
-            className={`${classes.formGroupContainer} ${classes.marginBottom}`}
-          >
-            <label htmlFor="password">Password:</label>
-            <div className={classes.inputWrapper}>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={classes.formGroupContainer}>
+              <label htmlFor="email">Email:</label>
               <input
-                type={inputType}
-                name="password"
-                id="password"
-                ref={(ref) => {
-                  passwordRef.current = ref;
-                  register(ref, { required: true, minLength: 8 });
-                }}
+                type="email"
+                name="email"
+                id="email"
+                ref={register({ required: true })}
               ></input>
-              {errors.password && errors.password.type === "required" && (
-                <span className={classes.error}>
-                  Password field is required.
-                </span>
-              )}
-              {inputType ? (
-                <EyeSlash
-                  className={classes.eyeslash}
-                  onClick={handleShowPassword}
-                />
-              ) : (
-                <Eye className={classes.eye} onClick={handleShowPassword} />
+              {errors.email && (
+                <span className={classes.error}>Email is required.</span>
               )}
             </div>
-          </div>
-          <div className={classes.resetPasswordLink} onClick={handleOpenModal}>
-            <p>Forgot password?</p>
-          </div>
-          <div className={classes.loginButton}>
-            <button className={classes.button} type="submit">
-              ʕ•́ᴥ•̀ʔっ Login
-            </button>
-          </div>
-        </form>
+            <PasswordInput
+              label="Password:"
+              errorRequired={true}
+              errorMinLength={true}
+              errorValidate={false}
+              name="password"
+            />
+            <div
+              className={classes.resetPasswordLink}
+              onClick={handleOpenModal}
+            >
+              <p>Forgot password?</p>
+            </div>
+            <div className={classes.loginButton}>
+              <button className={classes.button} type="submit">
+                ʕ•́ᴥ•̀ʔっ Login
+              </button>
+            </div>
+          </form>
+        </FormProvider>
       </div>
       <Modal
         isOpen={showModal}
