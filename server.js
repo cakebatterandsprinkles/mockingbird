89c8fbd2a562006020589187ffdf63112d0d7b75
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-27 15:12:04 -0400

    Production routing fix

--- server.js
+++ server.js
@@ -53,7 +53,12 @@ app.use(entryRoutes);
 
 // serve static files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build", "index.html")));
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handles any requests that don't match the ones above
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
 
 app.listen(PORT, () => console.log(`Backend server started on port: ${PORT}`));
